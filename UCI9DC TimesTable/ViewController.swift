67606937285df4ce0c83e409da168d1722250e45
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2016-02-17 14:41:24 +0500

    Improved performance and memory usage by making computations on the fly instead of precomputing on number change.

--- UCI9DC TimesTable/ViewController.swift
+++ UCI9DC TimesTable/ViewController.swift
@@ -12,22 +12,15 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var numberSlider: UISlider!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    private var number = 0
-    private var times = [String]()
     
     private let COUNT = 30
     private let NUMBER_DEF_KEY = "Number"
     
     @IBAction func numberChanged(sender: AnyObject) {
-        number = Int(numberSlider.value)
-        
         NSUserDefaults.standardUserDefaults().setFloat(numberSlider.value, forKey: NUMBER_DEF_KEY)
         
-        numberLabel.text = String(format: "%d", number)
-        times.removeAll()
-        for i in 2...COUNT {
-            times.append("\(number) x \(i) = \(i * number)")
-        }
+        numberLabel.text = String(format: "%d", Int(numberSlider.value))
+
         tableView.reloadData()
     }
 
@@ -37,27 +30,21 @@ class ViewController: UIViewController, UITableViewDataSource {
         if number >= numberSlider.minimumValue && number <= numberSlider.maximumValue {
             numberSlider.value = number
         }
-        // Do any additional setup after loading the view, typically from a nib.
         numberChanged(self)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("TimesOfNumber")!
         
-        if (times.count > indexPath.row) {
-            cell.textLabel?.text = times[indexPath.row]
-        }
+        let m = indexPath.row + 1
+        let num = Int(numberSlider.value)
+        cell.textLabel?.text = "\(num) * \(m) = \(num * m)"
         
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return times.count
+        return COUNT
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
