9e785906564f40761441ab495b2823a3fa72b13e
Stanislav Sidelnikov <sidelnikov.stanislav@gmail.com>
2016-02-17 14:00:06 +0500

    Building times table and saving entered value.

--- UCI9DC TimesTable/ViewController.swift
+++ UCI9DC TimesTable/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    private var number = 0
+    private var times = [String]()
+    
+    private let COUNT = 30
+    private let NUMBER_DEF_KEY = "Number"
+    
+    @IBAction func numberChanged(sender: AnyObject) {
+        number = Int(numberSlider.value)
+        
+        NSUserDefaults.standardUserDefaults().setFloat(numberSlider.value, forKey: NUMBER_DEF_KEY)
+        
+        numberLabel.text = String(format: "%d", number)
+        times.removeAll()
+        for i in 2...COUNT {
+            times.append("\(number) x \(i) = \(i * number)")
+        }
+        tableView.reloadData()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let number = NSUserDefaults.standardUserDefaults().floatForKey(NUMBER_DEF_KEY)
+        if number >= numberSlider.minimumValue && number <= numberSlider.maximumValue {
+            numberSlider.value = number
+        }
         // Do any additional setup after loading the view, typically from a nib.
+        numberChanged(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TimesOfNumber")!
+        
+        if (times.count > indexPath.row) {
+            cell.textLabel?.text = times[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return times.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
 }
 
